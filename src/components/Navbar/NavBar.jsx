de0ac053baf3e0f22f23426c8d05f901248089cb
Asadulla Khatami <asadullakhatami@gmail.com>
2026-01-04 23:12:44 +0600

    toggle menu items

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import Link from './Link';
+import React, { useState } from "react";
+import Link from "./Link";
+import { Menu, X } from "lucide-react";
 
 const nameOfPaths = [
   {
@@ -44,23 +45,41 @@ const nameOfPaths = [
   },
 ];
 
-
 const NavBar = () => {
-    return (
-        <nav>
-            {/* <ul className='flex'>
-                {
-                    nameOfPaths.map(route => <li key={route.id} className='mr-10'> <a href={route.path}>{route.name}</a></li>)
-                }
-            </ul> */}
+  const [open, setOpen] = useState(false);
+  return (
+    <nav>
+      <ul
+        className={`lg:hidden absolute ${
+          open ? "top-12" : "-top-100"
+        }  duration-1000 px-10`}
+      >
+        {nameOfPaths.map((route) => (
+          <li>{route.name}</li>
+        ))}
+      </ul>
+      <div className="h-20 flex justify-between items-center px-10 bg-white relative">
+        <span
+          onClick={() => setOpen(!open)}
+          className="flex justify-center items-center"
+        >
+          {open ? (
+            <X className="lg:hidden"></X>
+          ) : (
+            <Menu className="lg:hidden"></Menu>
+          )}
+          <h1 className="text-3xl ml-4 uppercase">My navbar</h1>
+        </span>
 
-            <ul className='flex'>
-                {
-                    nameOfPaths.map(route => <Link key={route.id} route={route}></Link>)
-                }
-            </ul>
-        </nav>
-    );
+        <ul className="lg:flex hidden">
+          {nameOfPaths.map((route) => (
+            <Link key={route.id} route={route}></Link>
+          ))}
+        </ul>
+        <button>Buy Now</button>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
